e6137e70dc8d74d50c746048a8b78d3f9233ee98
Don Choi <dongha.choi@vanderbilt.edu>
2018-07-18 09:50:36 -0500

    new chargeDash class

---
+++ Scripts/Player/ChargeDash.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargeDash {
+
+    private Camera c;
+    private Rigidbody2D myRigidBody;
+    private float chargeMax;
+    private float chargeFactor;
+
+    public ChargeDash(Camera c, Rigidbody2D myRigidBody, float chargeMax, float chargeFactor)
+    {
+        this.c = c;
+        this.myRigidBody = myRigidBody;
+        this.chargeMax = chargeMax;
+        this.chargeFactor = chargeFactor;
+    }
+
+    public void chargeDash(float timer, Transform transform, Fuel currentFuel)
+    {
+        Vector2 mouseDirection;
+        float chargeDuration;
+
+        //Get Fixed time
+        chargeDuration = (Time.time - timer) / 0.4f;
+
+        //Get mouse position and convert to onscreen position
+        Vector2 p = c.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+
+        //direction from object to mouse cursor
+        mouseDirection = new Vector2(p.x - transform.position.x, p.y - transform.position.y).normalized;
+
+        //if charge duration exceeds chargeMax, charge duration equals charge Max
+        if (chargeDuration > chargeMax)
+        {
+            chargeDuration = chargeMax;
+        }
+
+        //takes time duration and determines distance and direction based on hold duration
+        myRigidBody.velocity = mouseDirection * chargeDuration * chargeFactor;
+
+        Time.timeScale = 1f;
+
+        currentFuel.decreaseFuel(25);
+    }
+
+    public void chargeInput(Transform transform, Fuel currentFuel, string chargeButton)
+    {
+        float timer = 0;
+        bool charging = false;
+
+        if (Input.GetButtonDown(chargeButton))
+        {
+            //start timer
+            timer = Time.time;
+
+            charging = true;
+
+            Time.timeScale = .4f;
+        }
+
+        if (Input.GetButton(chargeButton) && charging)
+        {
+            if ((Time.time - timer) / 0.4f > chargeMax)
+            {
+                chargeDash(timer, transform, currentFuel);
+                charging = false;
+            }
+        }
+
+        if (Input.GetButtonUp(chargeButton) && charging)
+        {
+            chargeDash(timer, transform, currentFuel);
+            charging = false;
+        }
+    }
+}
--- Scripts/Player/PlayerControl.cs
+++ Scripts/Player/PlayerControl.cs
@@ -23,25 +23,24 @@ public class PlayerControl : MonoBehaviour, IKillable {
     private float currentSpeed;
     private Rigidbody2D myRigidBody;
     private float timer;
-    private Vector2 mousePosition;
     private bool charging;
     private bool engineOn;
     private Fuel currentFuel;
-
+    private ChargeDash myChargeDash;
 
     // Use this for initialization
     void Start() {
         myRigidBody = GetComponent<Rigidbody2D>();
         engineOn = true;
         currentFuel = new Fuel(100);
+        myChargeDash = new ChargeDash(c, myRigidBody, chargeMax, chargeFactor);
     }
 
     void Update() {
 
-        //Debug.Log(currentFuel.getFuel());
         if (engineOn)
         {
-            chargeInput();
+            myChargeDash.chargeInput(transform, currentFuel, chargeButton);
             BasicControl();
         }
         if (Input.GetButtonDown("Engine Button"))
@@ -73,70 +72,6 @@ public class PlayerControl : MonoBehaviour, IKillable {
             MovespeedCap.checkSpeed(myRigidBody, maxSpeed);
         }
 
-    }
-
-    /*handles mouse down, hold, and release for charge dash. 
-     * On mouse down, start timer and say player is charging, slow down time
-     * On hold, execute charge dash if charging and goes over charge max time
-     * On release, execute charge dash
-     */
-    void chargeInput()
-    {
-        if (Input.GetButtonDown(chargeButton))
-        {
-            //start timer
-            timer = Time.time;
-
-            charging = true;
-
-            Time.timeScale = .4f;
-        }
-
-        if (Input.GetButton(chargeButton) && charging)
-        {
-            if ((Time.time - timer) / 0.4f > chargeMax)
-            {
-                chargeDash();
-                charging = false;
-            }
-        }
-
-        if (Input.GetButtonUp(chargeButton) && charging)
-        {
-            chargeDash();
-            charging = false;
-        }
-
-    }
-
-    //dash in direction of mouse and duration charge was held
-    void chargeDash()
-    {
-        Vector2 mouseDirection;
-        float chargeDuration;
-
-        //Get Fixed time
-        chargeDuration = (Time.time - timer) / 0.4f;
-
-        //Get mouse position and convert to onscreen position
-        mousePosition = Input.mousePosition;
-        Vector2 p = c.ScreenToWorldPoint(new Vector2(mousePosition.x, mousePosition.y));
-
-        //direction from object to mouse cursor
-        mouseDirection = new Vector2(p.x - transform.position.x, p.y - transform.position.y).normalized;
-
-        //if charge duration exceeds chargeMax, charge duration equals charge Max
-        if (chargeDuration > chargeMax)
-        {
-            chargeDuration = chargeMax;
-        }
-
-        //takes time duration and determines distance and direction based on hold duration
-        myRigidBody.velocity = mouseDirection * chargeDuration * chargeFactor;
-
-        Time.timeScale = 1f;
-
-        currentFuel.decreaseFuel(25);
     }
 
     //engine button that toggles on and off engine, increases gravity when engine is off
