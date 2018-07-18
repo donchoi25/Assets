f9cab1280b83e5b5e1121c5a23433aa285c6d5a2
Don Choi <dongha.choi@vanderbilt.edu>
2018-07-17 21:26:57 -0500

    enemy class and list created

--- Scripts/Enemies/Enemy.cs
+++ Scripts/Enemies/Enemy.cs
@@ -2,6 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy {
-   
+public class Enemy: IKillable
+{
+    public static int enemyCount = 0;
+    public static List<Enemy> enemyMasterList = new List<Enemy>();
+
+    public Enemy()
+    {
+        ++enemyCount;
+        enemyMasterList.Add(this);
+    }
+
+    ~Enemy()
+    {
+        --enemyCount;
+        enemyMasterList.Remove(this);
+    }
+
+   public void Kill(float velocity)
+    {
+
+    }
+
+    public GameObject findPlayer()
+    {
+        return GameObject.FindGameObjectWithTag("Player");
+    }
+
 }
---
+++ Scripts/General/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static List<Enemy> enemyMasterList;
+    Enemy test;
+    Enemy test2;
+
+    void Start()
+    {
+        test = new Enemy();
+        test2 = new Enemy();
+        Debug.Log(Enemy.enemyMasterList);
+        Debug.Log(Enemy.enemyCount);
+    }
+
+    void Update()
+    {
+
+
+    }
+}
